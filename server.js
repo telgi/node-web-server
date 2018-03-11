d3a668871a19f1fb4c1b4d307b79c9611ea6c71c
Terry Allitt <tallitt88@gmail.com>
2018-03-11 22:12:06 +0000

    Play around with express routing, mostly get requests

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+var app = express();
+
+app.get('/', (req, res) => {
+  // res.send('<h1>Hello Express</h1>');
+  res.send({
+    name: 'Terry',
+    likes: [
+      'Betting',
+      'Coding'
+    ]
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.send('About Page')
+});
+
+app.get("/bad", (req, res) => {
+  res.send({
+    error: 'Oops. Something went wrong.'
+  })
+})
+
+app.listen(3000);
